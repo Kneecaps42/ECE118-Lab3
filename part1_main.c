c5cd44de5da971cf4ab2f606f059d216a6610db9
Jacob Long <jlong10@ucsc.edu>
2026-04-27 16:15:18 -0700

    part1 mostly done

--- part1_main.c
+++ part1_main.c
@@ -35,11 +35,44 @@ int main(void)
     
     while(1)
     {
+        //if new data is avail
+        if(AD_IsNewDataReady() == TRUE) {
+            
+            //read and store pin value
+            unsigned int adcRaw = AD_ReadADPin(AD_PORTW3); 
+            unsigned int adcTrimmed = (adcRaw >> 6) & 0x0F;
+            unsigned int LEDTrimmed = (adcRaw >> 7) & 0x0F;
         
+            
+            //conversion from ad value to pwm 
+            //1500 middle, +-45 = 400us range
+            //1100 + 7*5 * 53 = ~1500
+            //1500 - (7.5 * 53) = ~1100
+            //1500 + (7.5 * 53) = ~1900
+            unsigned int pwmVal = 1100 + (adcTrimmed * 53);
+            
+            
+            //setting lED
+            unsigned int LEDPattern = 0x01;
+            for(int i = 0; i< LEDTrimmed;i++){
+             LEDPattern |= LEDPattern << 1;
+            }
+            
+            LED_SetBank(LED_BANK1, LEDPattern);
+            LED_SetBank(LED_BANK2, LEDPattern >>4 );
+            
+            
+            //LED_SetBank(LED_BANK3, adcTrimmed);
+            
+            //setting servo 
+            RC_SetPulseTime(RC_PORTX03, pwmVal);
+                     
+                     
     }
     
     BOARD_End();
     return 0;
 }
 
+}
 #endif
